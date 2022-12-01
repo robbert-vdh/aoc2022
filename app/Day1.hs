1d7d7c160702006d344ff7f292a7fdc345484895
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-12-01 14:24:38 +0100

    Add day 1 part 1

--- app/Day1.hs
+++ app/Day1.hs
@@ -1,4 +1,27 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+  !input <- parse <$> readFile "inputs/day-1.txt"
+
+  putStrLn "Part 1:"
+  print $! maximum (map sum input)
+
+parse :: String -> [[Int]]
+parse = map (map read) . splitOn "" . lines
+
+-- | Split a list on a separator. Doesn't include the separator.
+splitOn :: forall a. Eq a => a -> [a] -> [[a]]
+splitOn sep = dropWhile null . foldr go []
+  where
+    -- This may prepend an empty list to the output, hence the @dropWhile null@.
+    go :: a -> [[a]] -> [[a]]
+    -- Prepend at most one empty list when encountering a separator
+    go a ([] : accs) | a == sep = [] : accs
+    go a accs | a == sep = [] : accs
+    -- Otherwise the element should be prepended to the first accumulator list
+    go a [] = [[a]]
+    go a (acc : accs) = (a : acc) : accs
