8bbe12d14032a68fb362718eb544e7f705d3675e
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-12-03 13:40:55 +0100

    Add day 3 part 2

--- app/Day3.hs
+++ app/Day3.hs
@@ -5,6 +5,8 @@ module Main where
 import Data.Char (ord)
 import Data.IntSet (IntSet)
 import qualified Data.IntSet as S
+import Data.List (foldl1')
+import Data.List.Split (chunksOf)
 import Data.Maybe (fromJust)
 
 main :: IO ()
@@ -14,9 +16,14 @@ main = do
   putStrLn "Part 1:"
   print $! sum . map (sum . duplicates) $ input
 
+  putStrLn "\nPart 2:"
+  print $! sum . map (sum . duplicates') . chunksOf 3 $ input
+
 -- | Characters are converted to integers such that a is 1, z is 26, A is 27,
 -- and Z is 52.
 data Rucksack = Rucksack IntSet IntSet deriving (Show)
+
+-- * Part 1
 
 duplicates :: Rucksack -> [Int]
 duplicates (Rucksack l r) = S.toList (S.intersection l r)
@@ -37,3 +44,12 @@ parse = map pRucksack . lines
        in Rucksack
             (S.fromList . map (fromJust . priority) $ take n line)
             (S.fromList . map (fromJust . priority) $ drop n line)
+
+-- * Part 2
+
+-- | The same as 'duplicates', but for n entire rucksacks and without
+-- distinguishing between compartments.
+duplicates' :: [Rucksack] -> [Int]
+duplicates' xs = S.toList (foldl1' S.intersection $ map mergeRucksack xs)
+  where
+    mergeRucksack (Rucksack l r) = S.union l r
