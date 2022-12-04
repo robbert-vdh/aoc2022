476735fe32564973d7bb2ca2026933227f22c58c
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-12-04 22:33:22 +0100

    Add day 4 part 1

---
+++ app/Day4.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module Main where
+
+import Data.List.Split
+
+main :: IO ()
+main = do
+  !input <- parse <$> readFile "inputs/day-4.txt"
+
+  putStrLn "Part 1:"
+  print $! sum . map (\(l, r) -> fromEnum (symmetricalContains l r)) $ input
+
+-- * Part 1
+
+data Range = Range Int Int deriving (Show)
+
+-- | Check whether the first range contains the second one. This assumes the
+-- second number in the range is equal to or greater than the first number.
+contains :: Range -> Range -> Bool
+contains (Range lx ly) (Range rx ry) = lx <= rx && ry <= ly
+
+symmetricalContains :: Range -> Range -> Bool
+symmetricalContains l r = contains l r || contains r l
+
+parse :: String -> [(Range, Range)]
+parse = map pLine . lines
+  where
+    pLine (splitOn "," -> [l, r]) = (pRange l, pRange r)
+    pLine _ = error "Input line does not contain exactly one comma"
+
+    pRange (splitOn "-" -> [l, r]) = Range (read l) (read r)
+    pRange _ = error "Invalid input format"
