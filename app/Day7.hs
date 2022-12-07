046e130418ab230b196dd8a211f9fbbd109a613d
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-12-07 20:38:22 +0100

    Add parsing for day 7
    I tried to be clever. It wasn't worth it. Doing it the dumb way like
    this was much simpler.

---
+++ app/Day7.hs
@@ -0,0 +1,119 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE TupleSections #-}
+
+module Main where
+
+import Data.Foldable
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as M
+import Text.Parsec hiding (parse)
+import qualified Text.Parsec as Parsec
+
+main :: IO ()
+main = do
+  !input <- parse <$> readFile "inputs/day-7.txt"
+
+  print input
+
+-- * Part 1
+
+type DirectoryListing = HashMap String FileSystemNode
+data FileSystemNode
+  = -- | A file with a certain size
+    File Int
+  | -- | A directory possibly containing other named files and directories.
+    Dir DirectoryListing
+  deriving (Show)
+
+-- ** Parsing
+
+-- | Parse the command line output for the root file system. This implementation
+-- is pretty inefficient because I didn't feel like implementing a zipper and
+-- parsing this recursively without one proved to be too much work.
+parse :: String -> DirectoryListing
+parse =
+  foldl' mergeDirs M.empty
+    . fromRight'
+    . Parsec.runParser (many pCommandBlock <* eof) [] "input"
+  where
+    fromRight' (Right a) = a
+    fromRight' e = error $ "That's not quite...right ;))))) " <> show e
+
+type Parser = Parsec String DirStack
+
+-- | Paths are a stack of directories. In other words, @/@ becomes @[]@ and
+-- @/foo/bar/baz@ becomes @["baz", "bar", "foo"]@.
+type DirStack = [String]
+
+-- | Contains the output of one or more @cd@ commands followed by an @ls@. This
+-- is incredibly inefficient and creates a directory listing with zero or more
+-- leading directories leading up to the directory listing from the @ls@
+-- command.
+pCommandBlock :: Parser DirectoryListing
+pCommandBlock = do
+  -- We're parsing the @$ @ command prefix first and then after every command.
+  -- This allows us to avoid backtracking since the cd commands and ls start
+  -- with different letters
+  _ <- string "$ " *> sepEndBy pCd (string "$ ")
+  ancestorDirs <- getState
+
+  -- The listing needs to have the leading directories from @currentDir@
+  -- prepended to it
+  relativeListing <- pLs
+
+  return $! foldl' addParentDir relativeListing ancestorDirs
+  where
+    -- This modifies the parser's state to set the current path.
+    pCd :: Parser ()
+    pCd = string "cd " *> (pCdParent <|> pCdRoot <|> pCdSubdir)
+
+    pCdParent = (string ".." *> endOfLine) >> modifyState tail
+    pCdRoot = (char '/' *> endOfLine) >> putState []
+    pCdSubdir = manyTill anyChar endOfLine >>= \subdir -> modifyState (subdir :)
+
+-- | We can cheat a bit here because of the way the commands are structured. At
+-- the end of a "block" we either go up to the parent directory and continue
+-- parsing @$ cd ...@ commands from there, or the block ends with a @$ cd /@ in
+-- which case we should ascend all the way to the root file system and continue
+-- from there.
+data CdResult = EnterDir String | GoToParent | GoToRoot deriving (Show)
+
+-- | Parse the output of an @$ ls@ command. Directories will be created as empty
+-- directories.
+pLs :: Parser DirectoryListing
+pLs = string "ls" *> endOfLine *> pLsOutput
+  where
+    -- The @manyTill ... endOfLine@ already consumes the end of line character,
+    -- so this is a regular many instead of 'endSepBy'
+    pLsOutput = foldl' insertNode' M.empty <$> many (pDir <|> pFile)
+    pDir = (,Dir M.empty) <$> (string "dir " *> manyTill anyChar endOfLine)
+    pFile = (\size name -> (name, File $ read size)) <$> many1 digit <*> (char ' ' *> manyTill anyChar endOfLine)
+
+-- | Insert a node into the file system. If the node is a directory that already
+-- exists, then the directory's contents are merged recursively. If the node is
+-- a file that already exists this function diverges. This directory merging
+-- should not be needed for this puzzle, but better be safe than sorry.
+insertNode :: DirectoryListing -> String -> FileSystemNode -> DirectoryListing
+insertNode m name node = M.alter go name m
+  where
+    go :: Maybe FileSystemNode -> Maybe FileSystemNode
+    go (Just (Dir existingNodes))
+      -- Directories should be recursively merged if this ever comes up
+      | Dir newNodes <- node =
+          Just . Dir $ mergeDirs existingNodes newNodes
+    go (Just _) = error "Duplicate files or directories in tree"
+    go Nothing = Just node
+
+-- | 'insertNode', but in a format that can be easily used with 'foldl''.
+insertNode' :: HashMap String FileSystemNode -> (String, FileSystemNode) -> HashMap String FileSystemNode
+insertNode' m (name, node) = insertNode m name node
+
+-- | Recursively merge two directory listings. Diverges when a regular file
+-- exists in both file systems or a path is a regular file in one file system
+-- and a directory in the other.
+mergeDirs :: DirectoryListing -> DirectoryListing -> DirectoryListing
+mergeDirs = M.foldlWithKey' insertNode
+
+-- | Wrap a directory listing in a parent directory.
+addParentDir :: DirectoryListing -> String -> DirectoryListing
+addParentDir m name = M.singleton name (Dir m)
