7a296cc952b6b0a83857aac4aa7af6bbfc899bf4
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-12-14 00:31:12 +0100

    Add day 13 part 1

--- app/Day13.hs
+++ app/Day13.hs
@@ -12,11 +12,41 @@ main = do
   !input <- parse <$> readFile "inputs/day-13.txt"
 
   putStrLn "Part 1:"
-  print input
+  print $! score $ map (uncurry verifyOrder) input
 
 -- * Part 1
 
 data Packet = PacketInt Int | PacketList [Packet] deriving (Show)
+
+-- | Determine the final part 1 score for a list of 'verifyOrder' results
+score :: [Bool] -> Int
+score = sum . zipWith (\idx result -> if result then idx else 0) [1..]
+
+-- | Check whether the two packets are in the correct order according to the
+-- bizarre rules from the puzzle.
+verifyOrder :: Packet -> Packet -> Bool
+verifyOrder l r = case verifyOrder' l r of
+  Just result -> result
+  Nothing -> error "Well uh yeah"
+
+-- | Returns @Just True@ if the inputs are in the right order, @Just False@ if
+-- they are not, and @Nothing@ if this was yet identified and the comparison
+-- algorithm should continue with the next element.
+verifyOrder' :: Packet -> Packet -> Maybe Bool
+verifyOrder' (PacketInt l) (PacketInt r)
+  | l < r = Just True
+  | l > r = Just False
+  | otherwise = Nothing
+-- When two lists are identical parsing should continue as normal
+verifyOrder' (PacketList []) (PacketList []) = Nothing
+verifyOrder' (PacketList []) (PacketList _) = Just True
+verifyOrder' (PacketList _) (PacketList []) = Just False
+verifyOrder' (PacketList (l : ls)) (PacketList (r : rs)) =
+  verifyOrder' l r <|> verifyOrder' (PacketList ls) (PacketList rs)
+verifyOrder' ls@(PacketList _) (PacketInt r) =
+  verifyOrder' ls (PacketList [PacketInt r])
+verifyOrder' (PacketInt l) rs@(PacketList _) =
+  verifyOrder' (PacketList [PacketInt l]) rs
 
 -- ** Parsing
 
