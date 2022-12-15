0f29f5cfff0b811122a7e50533471c898195ec25
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-12-15 16:46:29 +0100

    Add day 15 part 2

--- app/Day15.hs
+++ app/Day15.hs
@@ -2,8 +2,8 @@
 
 module Main where
 
-import Data.List (sort)
-import Data.Maybe (mapMaybe)
+import Data.List
+import Data.Maybe
 import Data.Void
 import Text.Megaparsec hiding (parse)
 import qualified Text.Megaparsec as Parsec
@@ -15,6 +15,9 @@ main = do
 
   putStrLn "Part 1:"
   print $! sum . map rangeDistance . mergeRanges . mapMaybe (scanRangeCoveringYCoord targetYCoord) $ input
+
+  putStrLn "\nPart 2"
+  print $! map tuningFrequency $! searchNonOverlappedPoints input
 
 -- * Part 1
 
@@ -39,7 +42,8 @@ mergeRanges = go . sort
     go [] = []
     go [x] = [x]
     go (r1@(startL, endL) : r2@(startR, endR) : rs)
-      | startR <= endL = go ((startL, max endL endR) : rs)
+      -- Note the +1, the range endpoints are inclusive
+      | startR <= endL + 1 = go ((startL, max endL endR) : rs)
       | otherwise = r1 : go (r2 : rs)
 
 -- | The distance covered by a range.
@@ -98,3 +102,64 @@ pPoint = (,) <$> (string "x=" *> pInt <* string ", ") <*> (string "y=" *> pInt)
 
 pSensorBeaconPair :: Parser SensorBeaconPair
 pSensorBeaconPair = SensorBeaconPair <$> (string "Sensor at " *> pPoint) <*> (string ": closest beacon is at " *> pPoint)
+
+-- * Part 2
+
+-- | The minimum X and Y coordinates to search in for part 2.
+searchXYMin, searchXYMax :: Int
+searchXYMin = 0
+searchXYMax = 4000000
+
+-- | The weird scoring metric from part 2 of the puzzle.
+tuningFrequency :: Point -> Int
+tuningFrequency (x, y) = (x * 4000000) + y
+
+-- | Find non-overlapped points in @[searchXYMin, searchXYMax]@ along both axes
+-- where the distress beacon could lie.
+searchNonOverlappedPoints :: [SensorBeaconPair] -> [Point]
+searchNonOverlappedPoints xs = concatMap candidatePointsForYCoord yRange
+  where
+    -- The range is the same across both axes, but since we're reusing the
+    -- horizontal slice functions from part 1 the x-range is a two point range
+    -- and the y-range is a list of points we'll map over
+    xRange = (searchXYMin, searchXYMax)
+    yRange = [searchXYMin .. searchXYMax]
+
+    candidatePointsForYCoord :: Int -> [Point]
+    candidatePointsForYCoord y =
+      let !ranges = mergeRanges $ mapMaybe (scanRangeCoveringYCoord y) xs
+          candidates = rangeDifferences xRange ranges
+       in concatMap (\(startX, endX) -> [(x, y) | x <- [startX .. endX]]) candidates
+
+-- | Return the parts in the left range that are not present in the right
+-- ranges. This is not a symmetrical difference. These ranges are still
+-- inclusive
+rangeDifferences :: (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+rangeDifferences range = rangeIntersections . concatMap (rangeDifference range)
+
+rangeDifference :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
+rangeDifference (startL, endL) (startR, endR) = catMaybes [startSegment, endSegment]
+  where
+    startSegment =
+      if startL < startR
+        then Just (startL, min endL (startR - 1))
+        else Nothing
+    endSegment =
+      if endR < endL
+        then Just (max startL (endR + 1), endL)
+        else Nothing
+
+rangeIntersections :: [(Int, Int)] -> [(Int, Int)]
+rangeIntersections = pairwiseIntersect . sort
+  where
+    pairwiseIntersect :: [(Int, Int)] -> [(Int, Int)]
+    pairwiseIntersect ranges = mapMaybe (uncurry rangeIntersect) (zip ranges $ tail ranges)
+
+    -- We already know the ranges are sorted, so we can skip some min/max operations
+    rangeIntersect :: (Int, Int) -> (Int, Int) -> Maybe (Int, Int)
+    rangeIntersect (startL, endL) (startR, endR) =
+      let start = max startL startR
+          end = min endL endR
+       in if start <= end
+            then Just (start, end)
+            else Nothing
